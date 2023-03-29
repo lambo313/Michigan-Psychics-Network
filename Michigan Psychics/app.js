458b2c88bb8f4827fceb796d252656fb77048dbc
Cedric <128867379+lambo313@users.noreply.github.com>
2023-03-29 00:27:12 -0400

    Add files via upload
    Fixed faq. Glass design update in progress

--- Michigan Psychics/app.js
+++ Michigan Psychics/app.js
@@ -1,4 +1,4 @@
- /* When the user clicks on the button, 
+/* When the user clicks on the button, 
   toggle between hiding and showing the dropdown content */
 
   function myFunction2() {
@@ -9,32 +9,49 @@
   //   document.getElementById("myDropdown3").classList.toggle("show");
   // }
 
-  const faq = document.querySelectorAll(".faq");
-  
-  faq.forEach((item, index) => {
-    let header = item.querySelector(".faq header");
-    header.addEventListener("click", () => {
-      item.classList.toggle("open");
 
-      let description = item.querySelector(".faq .description");
-      if(item.classList.contains(open)){
-        description.style.display = ("flex");
-      } else  {
-        description.style.display = "none";
-      } 
+ let li = document.querySelectorAll(".faq-all li");
+ for(var i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", (e)=> {
+    let clickedLi;
+    if(e.target.classList.contains("faq"))
+      clickedLi = e.target.parentElement;
+      else {
+       clickedLi = e.target.parentElement.parentElement;
+       console.log(clickedLi);
+    }
+    clickedLi.classList.toggle("description")
+  });
+}
 
-      if(description.style.display = "flex"){
-        let description = item.querySelector(".faq .description");
-      if(item.classList.contains(open)){
-        description.style.display = ("none");
-      } else  {
-        description.style.display = "flex";
-      } 
-      }
-      console.log(description);
-    })
     
-  })
+      
+
+  
+  // faq.forEach((item, index) => {
+  //   let header = item.querySelector(".faq");
+  //   header.addEventListener("click", () => {
+  //     item.classList.toggle("open");
+
+  //     let description = item.querySelector(".faq-all .description");
+  //     if(item.classList.contains(open)){
+  //       description.style.display = ("flex");
+  //     } else  {
+  //       description.style.display = "none";
+  //     } 
+
+  //     if(description.style.display = "flex"){
+  //       let description = item.querySelector(".faq-all .description");
+  //     if(item.classList.contains(open)){
+  //       description.style.display = ("none");
+  //     } else  {
+  //       description.style.display = "flex";
+  //     } 
+  //     }
+  //     console.log(description);
+  //   })
+    
+  // })
 
   // Close the dropdown if the user clicks outside of it
   window.onclick = function(e) {
@@ -42,7 +59,7 @@
     if (!e.target.matches('.dropbtn2')) {
     var myDropdown2 = document.getElementById("myDropdown2");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (i = 0; i < myDropdown2.length; i++) {
       var openDropdown = dropdowns[i];
       if (myDropdown2.classList.contains('show')) {
         myDropdown2.classList.remove('show');
@@ -50,14 +67,5 @@
     }
    }
 
-  //  if (!e.target.matches('.faq')) {
-  //   var myDropdown3 = document.getElementById("myDropdown3");
-  //   var i;
-  //   for (i = 0; i < dropdowns.length; i++) {
-  //     var openDropdown = dropdowns[i];
-  //     if (myDropdown3.classList.contains('show')) {
-  //       myDropdown3.classList.remove('show');
-  //     }
-  //   }
-  //  }
+
   }
